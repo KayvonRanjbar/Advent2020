e81cadb1a7a65c2603860d442f34ddc6e4913798
kranjbar <kayvon.ranjbar@gmail.com>
2020-12-22 21:07:18 -0700

    Day 9: Part 1

--- Advent2020/Program.cs
+++ Advent2020/Program.cs
@@ -9,7 +9,45 @@ namespace Advent2020
     {
         static void Main(string[] args)
         {
-            string[] instructions = File.ReadAllLines("Instructions.txt");
+            string[] numbers = File.ReadAllLines("Inputs/Numbers.txt");
+
+            const int amountOfPreviousNumbers = 25;
+
+            int? result = FirstNumberNotSumOfPreviousNumbers(numbers, amountOfPreviousNumbers);
+
+            Console.WriteLine(result);
+        }
+
+        private static int? FirstNumberNotSumOfPreviousNumbers(string[] numbers, in int amountOfPreviousNumbers)
+        {
+            int? firstNumberNotSumOfPreviousNumbers = default;
+
+            for (int i = amountOfPreviousNumbers; i < numbers.Length; i++)
+            {
+                int nextNumber = int.Parse(numbers[i]);
+                List<int> previousNumbers =
+                    numbers.Take(i).TakeLast(amountOfPreviousNumbers).Select(int.Parse).ToList();
+                if (!IsNumberSumOfPreviousNumbers(nextNumber, previousNumbers))
+                {
+                    firstNumberNotSumOfPreviousNumbers = nextNumber;
+                    break;
+                }
+            }
+
+            return firstNumberNotSumOfPreviousNumbers;
+        }
+
+        private static bool IsNumberSumOfPreviousNumbers(int nextNumber, List<int> previousNumbers)
+        {
+            foreach (int addend in previousNumbers)
+            {
+                int[] previousNumbersWithoutAddend = new int[previousNumbers.Count];
+                previousNumbers.CopyTo(previousNumbersWithoutAddend);
+                if (previousNumbersWithoutAddend.Any(n => n == nextNumber - addend))
+                    return true;
+            }
+
+            return false;
         }
     }
 }
