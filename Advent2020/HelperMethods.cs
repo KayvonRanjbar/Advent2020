ab15ce5ef3625ea2c68484e10541c0c3a331c053
kranjbar <kayvon.ranjbar@gmail.com>
2020-12-23 11:04:34 -0700

    Hide Day 9 Part 2

--- Advent2020/HelperMethods.cs
+++ Advent2020/HelperMethods.cs
@@ -147,5 +147,39 @@ namespace Advent2020
 
             return false;
         }
+
+        public static long FindEncryptionWeakness(List<long> numbers, in long invalidNumber)
+        {
+            List<long> contiguousRange = FindContiguousRangeThatSumsToInvalidNumber(numbers, invalidNumber);
+
+            long encryptionWeakness = contiguousRange.Min() + contiguousRange.Max();
+
+            return encryptionWeakness;
+        }
+
+        private static List<long> FindContiguousRangeThatSumsToInvalidNumber(List<long> numbers, in long invalidNumber)
+        {
+            List<long> contiguousRange = new List<long>();
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                contiguousRange = new List<long>();
+                contiguousRange.Add(numbers[i]);
+                long sum = contiguousRange.Sum();
+                
+                for (int j = i + 1; sum < invalidNumber; j++)
+                {
+                    contiguousRange.Add(numbers[j]);
+                    sum = contiguousRange.Sum();
+                }
+
+                if (sum == invalidNumber)
+                {
+                    return contiguousRange;
+                }
+            }
+            
+            throw new Exception("The contiguous range was not found");
+        }
     }
 }
