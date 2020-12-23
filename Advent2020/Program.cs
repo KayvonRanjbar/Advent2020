62ff6373114dfdfdb983741db6d54896a04d20e3
kranjbar <kayvon.ranjbar@gmail.com>
2020-12-22 22:01:54 -0700

    Day 9 Part 2

--- Advent2020/Program.cs
+++ Advent2020/Program.cs
@@ -9,7 +9,47 @@ namespace Advent2020
     {
         static void Main(string[] args)
         {
-            string[] numbers = File.ReadAllLines("Inputs/Numbers.txt");
+            List<long> numbers = File.ReadAllLines("Inputs/Numbers.txt").Select(long.Parse).ToList();
+
+            const long invalidNumber = 25918798;
+
+            long encryptionWeakness = FindEncryptionWeakness(numbers, invalidNumber);
+
+            Console.WriteLine(encryptionWeakness);
+        }
+
+        private static long FindEncryptionWeakness(List<long> numbers, in long invalidNumber)
+        {
+            List<long> contiguousRange = FindContiguousRangeThatSumsToInvalidNumber(numbers, invalidNumber);
+
+            long encryptionWeakness = contiguousRange.Min() + contiguousRange.Max();
+
+            return encryptionWeakness;
+        }
+
+        private static List<long> FindContiguousRangeThatSumsToInvalidNumber(List<long> numbers, in long invalidNumber)
+        {
+            List<long> contiguousRange = new List<long>();
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                contiguousRange = new List<long>();
+                contiguousRange.Add(numbers[i]);
+                long sum = contiguousRange.Sum();
+                
+                for (int j = i + 1; sum < invalidNumber; j++)
+                {
+                    contiguousRange.Add(numbers[j]);
+                    sum = contiguousRange.Sum();
+                }
+
+                if (sum == invalidNumber)
+                {
+                    return contiguousRange;
+                }
+            }
+            
+            throw new Exception("The contiguous range was not found");
         }
     }
 }
