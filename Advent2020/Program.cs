c7623167e423cbdaedbc1a0e688633f9d60035da
kranjbar <kayvon.ranjbar@gmail.com>
2020-12-23 11:56:05 -0700

    Day 10 Part 1

--- Advent2020/Program.cs
+++ Advent2020/Program.cs
@@ -9,6 +9,48 @@ namespace Advent2020
     {
         static void Main(string[] args)
         {
+            // What is the number of 1-jolt differences multiplied by the number of 3-jolt differences?
+            
+            IEnumerable<long> adapterJoltageRatings =
+                File.ReadAllLines("Inputs/Joltage_ratings.txt").Select(long.Parse);
+
+            List<long> sortedAdapterJoltageRatings = adapterJoltageRatings.OrderBy(r => r).ToList();
+            
+            CountJoltageDifferences(sortedAdapterJoltageRatings, out long OneJoltDifferences, out long ThreeJoltDifferences);
+            
+            long productOfOneJoltDifferencesAndThreeJoltDifferences = OneJoltDifferences * ThreeJoltDifferences;
+
+            Console.WriteLine(productOfOneJoltDifferencesAndThreeJoltDifferences);
+        }
+
+        private static void CountJoltageDifferences(List<long> sortedAdapterJoltageRatings,
+            out long oneJoltDifferences, out long threeJoltDifferences)
+        {
+            oneJoltDifferences = 0;
+            threeJoltDifferences = 1; // Start with always-there device joltage difference
+
+            switch (sortedAdapterJoltageRatings.First())
+            {
+                case 1:
+                    oneJoltDifferences++;
+                    break;
+                case 3:
+                    threeJoltDifferences++;
+                    break;
+            }
+            
+            for (int i = 0; i < sortedAdapterJoltageRatings.Count - 1; i++)
+            {
+                long difference = sortedAdapterJoltageRatings[i + 1] - sortedAdapterJoltageRatings[i];
+                if (difference == 1)
+                {
+                    oneJoltDifferences++;
+                }
+                else if (difference == 3)
+                {
+                    threeJoltDifferences++;
+                }
+            }
         }
     }
 }
