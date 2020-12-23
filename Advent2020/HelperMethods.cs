a1b8971c00a57c6a03f545bb17dfde6b8b9551a7
kranjbar <kayvon.ranjbar@gmail.com>
2020-12-22 21:10:07 -0700

    Hide Day 9-1

--- Advent2020/HelperMethods.cs
+++ Advent2020/HelperMethods.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Advent2020
 {
@@ -113,6 +114,38 @@ namespace Advent2020
                     throw new Exception($"Invalid operation: {operation}");
                 }
             }
+        }
+
+        public static int? FirstNumberNotSumOfPreviousNumbers(string[] numbers, in int amountOfPreviousNumbers)
+        {
+            int? firstNumberNotSumOfPreviousNumbers = default;
+
+            for (int i = amountOfPreviousNumbers; i < numbers.Length; i++)
+            {
+                int nextNumber = int.Parse(numbers[i]);
+                List<int> previousNumbers =
+                    numbers.Take(i).TakeLast(amountOfPreviousNumbers).Select(int.Parse).ToList();
+                if (!IsNumberSumOfPreviousNumbers(nextNumber, previousNumbers))
+                {
+                    firstNumberNotSumOfPreviousNumbers = nextNumber;
+                    break;
+                }
+            }
+
+            return firstNumberNotSumOfPreviousNumbers;
+        }
+
+        private static bool IsNumberSumOfPreviousNumbers(int nextNumber, List<int> previousNumbers)
+        {
+            foreach (int addend in previousNumbers)
+            {
+                int[] previousNumbersWithoutAddend = new int[previousNumbers.Count];
+                previousNumbers.CopyTo(previousNumbersWithoutAddend);
+                if (previousNumbersWithoutAddend.Any(n => n == nextNumber - addend))
+                    return true;
+            }
+
+            return false;
         }
     }
 }
